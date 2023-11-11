22c73df401699efcdc586aeba7f36765906f719a
league <mclaren.grant12@gmail.com>
2023-11-10 16:53:08 -0800

    fdseww

--- src/_01_array_list_visualizer/VisualArrayLists.java
+++ src/_01_array_list_visualizer/VisualArrayLists.java
@@ -16,21 +16,29 @@ public class VisualArrayLists implements ActionListener {
 
 	public void addThingsToList() {
 		// 1. Add a bunch of items to our list (4 or 5 should be enough!)
-		
+		list.add("organism");
+		list.add("pythagorean theorruy");
+		list.add("ratatoullie");
+		list.add("tom and jerry");
 		// 2. Set one item in our list to be something different
-		
+		list.set(1, "the lourve");
 		// 3. Remove something from our list
-		
+		list.remove(0);
 		// 4. Print the size of our list
-		
+		System.out.println(list.size());
 		// 5. Check if our list contains a specific thing of your choice
 		//	  and print whether it is in the list or not.
-		
+		if(list.contains("the lourve")) {
+			System.out.println("aaaur they've done it again, 'VIVA LA REVOLUTION' ");
+		} else {
+			System.out.println("the mona lisa has emergeddd.");
+		}
 		// 6. Get a specific item from our list and print it out
-		
+		System.out.println(list.get(2));
 		// 7. Clear our list of all items
-		
+		list.clear();
 		// 8. Print the size of our list again
+		System.out.println(list.size());
 	}
 	
 	
--- src/_02_array_list_guestbook/GuestBook.java
+++ src/_02_array_list_guestbook/GuestBook.java
@@ -1,12 +1,70 @@
 package _02_array_list_guestbook;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
-	// Guest #1: Bob Banders
-	// Guest #2: Sandy Summers
-	// Guest #3: Greg Ganders
-	// Guest #4: Donny Doners
+	
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button1 = new JButton();
+	JButton button = new JButton();
+	ArrayList<String> names = new  ArrayList<String>();
+	
+	public static void main(String[] args) {
+		GuestBook book = new GuestBook();
+		book.run();
+	}
+	
+	void run() {
+		names.add( "Guest #1: Bob Banders");
+		names.add(" Guest #2: Sandy Summers");
+		names.add( " Guest #3: Greg Ganders");
+		names.add("Guest #4: Donny Doners");
+	////////////////////////////////////////////
+		frame.setVisible(true);
+		frame.add(panel);
+		panel.add(button1);
+		panel.add(button);
+		button.setText("Add Name");
+		button1.setText("view Names");
+		button.addActionListener(this);
+		button1.addActionListener(this);
+		frame.setTitle("GuestBook");
+		frame.pack();
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton buttons = (JButton) e.getSource();
+		
+		if(button == buttons) {
+			String input =	JOptionPane.showInputDialog(null, "please enter name.");
+			if( (!input.equals(null)) && (!input.isEmpty()) ) {
+				names.add("Guest #"+(names.size()+1)+": "+input);
+			} else {
+				JOptionPane.showMessageDialog(null, "name not entered");
+			}
+		}
+			
+	
+		
+		if(button1 == buttons) {
+			JOptionPane.showMessageDialog(null, "current Guest list: "+names);
+			
+			}
+		
+	}
 }
